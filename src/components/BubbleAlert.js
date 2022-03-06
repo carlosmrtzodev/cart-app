d2a89251490a3f2e4915f77727159974106eb2e4
DNC\carlo <carlosmrtzow8@gmail.com>
2022-03-06 06:06:41 -0400

    push to a repository in github

---
+++ src/components/BubbleAlert.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+const styles = {
+  bubbleAlert: {
+    backgroundColor: "#e9725a",
+    borderRadius: "15px",
+    color: "#fff",
+    padding: "2px 10px",
+    fontSize: "0.9rem",
+    width: "20px",
+  },
+};
+class BubbleAlert extends Component {
+  getNumber(n) {
+    if (!n) {
+      return "";
+    }
+    return n > 9 ? "9+" : n;
+  }
+  render() {
+    const { value } = this.props;
+    console.log(value);
+    return <span style={styles.bubbleAlert}>{this.getNumber(value)}</span>;
+  }
+}
+
+export default BubbleAlert;
